f32f867dde339a693f494428074773f90c5ade56
Brian Woodward <brian.woodward@gmail.com>
2014-10-17 11:54:59 -0400

    forked and setup

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * codemash-speakers <https://github.com/doowb/codemash-speakers>
+ *
+ * Copyright (c) 2014 Brian Woodward, contributors.
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function () {
+  // do stuff
+};
